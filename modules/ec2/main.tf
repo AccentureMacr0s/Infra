d4754cb1d2281b855e2f58d7bc4ca7e5a563c0a4
mr.smaill213@gmail.com <mr.smaill213@gmail.com>
2024-01-20 16:06:55 +0200

    Added some examples of the module to use

---
+++ modules/ec2/main.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "example" {
+  name        = "example_security_group"
+  description = "Security group for example usage"
+  vpc_id      = aws_vpc.example.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ modules/ssm/main.tf
@@ -0,0 +1,5 @@
+module "ssm_association" {
+  source = "terraform-aws-modules/ssm/aws//modules/association"
+
+  # ... настройки модуля, например, имена ассоциаций, документы SSM и т.д.
+}
